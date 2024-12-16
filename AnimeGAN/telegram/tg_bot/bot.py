2b59421fe4a2db6934b2c52d3dac17caad11d505
Andrey Klimin <andrey7klimin@yandex.ru>
2024-12-16 11:37:20 +0300

    download user's files

--- AnimeGAN/telegram/tg_bot/bot.py
+++ AnimeGAN/telegram/tg_bot/bot.py
@@ -19,18 +19,33 @@ async def send_welcome(message: aiogram.types.Message):
 @db.message(aiogram.F.content_type == "photo")
 async def get_image(message: aiogram.types.Message):
     await message.reply_photo(message.photo[-1].file_id)
+    file = await message.bot.get_file(message.photo[-1].file_id)
+    await bot.download_file(file.file_path, r"AnimeGAN/telegram/tg_bot/photo/" + str(message.photo[-1].file_id))
 
 @db.message(aiogram.F.content_type == "video_note")
 async def get_video_note(message: aiogram.types.Message):
     await message.reply_video_note(message.video_note.file_id)
+    file = await message.bot.get_file(message.video_note.file_id)
+    await bot.download_file(file.file_path, r"AnimeGAN/telegram/tg_bot/video_note/" + str(message.video_note.file_id))
 
 @db.message(aiogram.F.content_type == "video")
 async def get_video(message: aiogram.types.Message):
     await message.reply_video(message.video_note.file_id)
+    file = await message.bot.get_file(message.video.file_id)
+    await bot.download_file(file.file_path, r"AnimeGAN/telegram/tg_bot/video/" + str(message.video.file_id))
 
 @db.message()
 async def echo(message: aiogram.types.Message):
     await message.answer(message.text)
 
+def make_dir():
+    if not os.path.exists(r"AnimeGAN/telegram/tg_bot/photo/"):
+        os.mkdir("AnimeGAN/telegram/tg_bot/photo/")
+    if not os.path.exists(r"AnimeGAN/telegram/tg_bot/video/"):
+        os.mkdir("AnimeGAN/telegram/tg_bot/video/")
+    if not os.path.exists(r"AnimeGAN/telegram/tg_bot/video_note/"):
+        os.mkdir("AnimeGAN/telegram/tg_bot/video_note/")
+
 def start_bot():
+    make_dir()
     asyncio.run(main())
