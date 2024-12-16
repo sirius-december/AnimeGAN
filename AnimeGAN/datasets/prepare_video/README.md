4a4458c8b044b28aa1aca46084e54d32c948fcf6
Maxim <max.sherb2007@gmail.com>
2024-12-16 12:04:34 +0300

    Updated prepare_images

--- AnimeGAN/datasets/prepare_video/README.md
+++ AnimeGAN/datasets/prepare_video/README.md
@@ -5,15 +5,12 @@ Script takes video, extracts frames with given step and resizes them
 ### Usage
 `python prepare_images.py -h` for help
 
-`python prepare_images.py -s <start-frame> -e <end-frame> -i <input> -o <output> -d <frame-distance> -w <width> -l <height>`
+`python prepare_images.py -s <start-second> -e <end-second> -i <input> -o <output> -d <frame-distance> -w <width> -l <height>`
 
-- `-s` / `--start-frame` – first frame of the video which be processed. Should be in range [1, `cnt`], where `cnt` is total frames in the video.
-- `-e` / `--end-frame` – last frame of the video which be processed. Could not result in image if `e - s` is not divisible by `frame-distance`. Should be in range [`s`, `cnt`], where `s` is start frame and `cnt` is total frames in the video.
-- `-i` / `--input` – absolute path to video being processed.
-- `-o` / `--output` – path to directory, in which output images would be stored.
-- `-d` / `--frame-distance` – distance between frames being saved. Should be positive integer.
-- `-w` / `--width` – width of output images. Should be positive integer.
-- `-l` / `--height` – height of output images. Should be positive integer.
-
-All arguments are required.
-
+- `-s` / `--start-second` **required** – first second of the video which be processed. Should be in range positive integer.
+- `-e` / `--end-second` **required** – last second of the video which be processed. Should be not less than start second. Cuts to last second of the video if larger.
+- `-i` / `--input` **required**  – absolute path to video being processed.
+- `-o` / `--output` **required** – path to directory, in which output images would be stored.
+- `-d` / `--frame-distance` **required** – distance between frames being saved. Should be positive integer.
+- `-w` / `--width` – width of output images. Should be positive integer. Frame width by default.
+- `-l` / `--height` – height of output images. Should be positive integer. Frame height by default.
--- AnimeGAN/datasets/prepare_video/prepare_images.py
+++ AnimeGAN/datasets/prepare_video/prepare_images.py
@@ -1,39 +1,30 @@
 import cv2
 import os
 import argparse
+from tqdm import tqdm
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog="Prepare Images",
         description="A script to cut images <width> by <height> pixels from <input> prepare_video from "
-                    "<start-frame> to <end-frame> with step <frame-distance> and save it"
+                    "<start-second> to <end-second> with step <frame-distance> and save it"
                     "to <output> directory"
     )
-    parser.add_argument('-s', '--start-frame', required=True, type=int)
-    parser.add_argument('-e', '--end-frame', required=True, type=int)
+    parser.add_argument('-s', '--start-second', required=True, type=int)
+    parser.add_argument('-e', '--end-second', required=True, type=int)
     parser.add_argument('-i', '--input', required=True)
     parser.add_argument('-o', '--output', required=True)
     parser.add_argument('-d', '--frame-distance', required=True, type=int)
-    parser.add_argument('-w', '--width', required=True, type=int)
-    parser.add_argument('-l', '--height', required=True, type=int)
+    parser.add_argument('-w', '--width', required=False, type=int)
+    parser.add_argument('-l', '--height', required=False, type=int)
 
     args = parser.parse_args()
 
     VIDEO_PATH = args.input
-    START_FRAME = args.start_frame
-    END_FRAME = args.end_frame
-    OUT_WIDTH = args.width
-    OUT_HEIGHT = args.height
+    START_SECOND = args.start_second
+    END_SECOND = args.end_second
     FRAME_DISTANCE = args.frame_distance
     OUT_DIR = args.output
-
-    if OUT_WIDTH <= 0:
-        print("Image width should be positive")
-        exit(-1)
-
-    if OUT_HEIGHT <= 0:
-        print("Image height should be positive")
-        exit(-1)
 
     if FRAME_DISTANCE <= 0:
         print("Frame distance should be positive")
@@ -45,35 +36,64 @@ if __name__ == '__main__':
         print("Error opening prepare_video")
         exit(-1)
 
+    OUT_WIDTH = args.width
+    OUT_HEIGHT = args.height
+
+    real_frame_width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+    real_frame_height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    needs_resize = (OUT_WIDTH is not None) or (OUT_HEIGHT is not None)
+
+    if OUT_WIDTH is None:
+        OUT_WIDTH = real_frame_width
+
+    if OUT_HEIGHT is None:
+        OUT_HEIGHT = real_frame_height
+
+    if OUT_WIDTH <= 0:
+        print("Image width should be positive")
+        exit(-1)
+
+    if OUT_HEIGHT <= 0:
+        print("Image height should be positive")
+        exit(-1)
+
     total_frames = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+    fps = capture.get(cv2.CAP_PROP_FPS)
 
-    if START_FRAME <= 0 or START_FRAME > total_frames:
-        print(f'Start frame should be in range [1, {total_frames}], because prepare_video have {total_frames} frames')
+    if START_SECOND <= 0:
+        print(f'Start second should be positive integer')
         exit(-1)
 
-    if END_FRAME < START_FRAME or END_FRAME > total_frames:
-        print(f'End frame should be in range [{START_FRAME}, {total_frames}], because prepare_video have {total_frames} '
-              f'frames and first frame is {START_FRAME}')
+    if END_SECOND < START_SECOND:
+        print(f'End second should be not less than start second')
         exit(-1)
+
+    START_FRAME = (START_SECOND - 1) * fps
+    END_FRAME = (END_SECOND - 1) * fps
+
+    START_FRAME = min(START_FRAME, total_frames)
+    END_FRAME = min(END_FRAME, total_frames)
 
     capture.set(cv2.CAP_PROP_POS_FRAMES, START_FRAME - 1)
     cur_frame = START_FRAME
 
     img_id = 0
 
-    while cur_frame <= END_FRAME:
+    total_iterations = int((END_FRAME - START_FRAME) // FRAME_DISTANCE) + 1
+
+    for i in tqdm(range(total_iterations)):
+        capture.set(cv2.CAP_PROP_POS_FRAMES, cur_frame - 1)
+
         ret, frame = capture.read()
         if not ret:
             break
 
-        if (cur_frame - START_FRAME) % FRAME_DISTANCE != 0:
-            cur_frame += 1
-            continue
+        if needs_resize:
+            frame = cv2.resize(frame, (OUT_WIDTH, OUT_HEIGHT))
 
-        cur_frame += 1
-
-        frame = cv2.resize(frame, (OUT_WIDTH, OUT_HEIGHT))
         cv2.imwrite(os.path.join(OUT_DIR, f'{img_id}.png'), frame)
+        cur_frame += FRAME_DISTANCE
         img_id += 1
 
     capture.release()
