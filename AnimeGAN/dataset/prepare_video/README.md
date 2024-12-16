9d3a97b37088c3bc73b4982e7cb90f1f13da9917
Maxim <max.sherb2007@gmail.com>
2024-12-16 11:31:55 +0300

    Documentation to script preparing videos

---
+++ AnimeGAN/dataset/prepare_video/README.md
@@ -0,0 +1,19 @@
+### Description
+
+Script takes video, extracts frames with given step and resizes them
+
+### Usage
+`python prepare_images.py -h` for help
+
+`python prepare_images.py -s <start-frame> -e <end-frame> -i <input> -o <output> -d <frame-distance> -w <width> -h <height>`
+
+- `-s` / `--start-frame` – first frame of the video which be processed. Should be in range [1, `cnt`], where `cnt` is total frames in the video.
+- `-e` / `--end-frame` – last frame of the video which be processed. Could not result in image if `e - s` is not divisible by `frame-distance`. Should be in range [`s`, `cnt`], where `s` is start frame and `cnt` is total frames in the video.
+- `-i` / `--input` – absolute path to video being processed.
+- `-o` / `--output` – path to directory, in which output images would be stored.
+- `-d` / `--frame-distance` – distance between frames being saved. Should be positive integer.
+- `-w` / `--width` – width of output images. Should be positive integer.
+- `-l` / `--height` – height of output images. Should be positive integer.
+
+All arguments are required.
+
--- AnimeGAN/dataset/prepare_video/prepare_images.py
+++ AnimeGAN/dataset/prepare_video/prepare_images.py
@@ -5,7 +5,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog="Prepare Images",
-        description="A script to cut images <width> by <height> pixels from <input> video from "
+        description="A script to cut images <width> by <height> pixels from <input> prepare_video from "
                     "<start-frame> to <end-frame> with step <frame-distance> and save it"
                     "to <output> directory"
     )
@@ -35,24 +35,24 @@ if __name__ == '__main__':
         print("Image height should be positive")
         exit(-1)
 
-    if FRAME_DISTANCE < 0:
+    if FRAME_DISTANCE <= 0:
         print("Frame distance should be positive")
         exit(-1)
 
     capture = cv2.VideoCapture(VIDEO_PATH)
 
     if not capture.isOpened():
-        print("Error opening video")
+        print("Error opening prepare_video")
         exit(-1)
 
     total_frames = capture.get(cv2.CAP_PROP_FRAME_COUNT)
 
     if START_FRAME <= 0 or START_FRAME > total_frames:
-        print(f'Start frame should be in range [1, {total_frames}], because video have {total_frames} frames')
+        print(f'Start frame should be in range [1, {total_frames}], because prepare_video have {total_frames} frames')
         exit(-1)
 
     if END_FRAME < START_FRAME or END_FRAME > total_frames:
-        print(f'End frame should be in range [{START_FRAME}, {total_frames}], because video have {total_frames} '
+        print(f'End frame should be in range [{START_FRAME}, {total_frames}], because prepare_video have {total_frames} '
               f'frames and first frame is {START_FRAME}')
         exit(-1)
 
