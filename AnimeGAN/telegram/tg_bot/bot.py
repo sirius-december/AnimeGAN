644d1f90a7357fe77060f6ded7a3dbd8f222e58f
Vlad Ogloblin <igrulichki24@gmail.com>
2024-12-16 12:05:52 +0300

    start_buttons

--- AnimeGAN/telegram/tg_bot/bot.py
+++ AnimeGAN/telegram/tg_bot/bot.py
@@ -6,20 +6,64 @@ import os
 dotenv.load_dotenv()
 
 bot = aiogram.Bot(os.environ["TOKEN"])
-db = aiogram.Dispatcher()
+dp = aiogram.Dispatcher()
 
 async def main():
-    await db.start_polling(bot)
+    await dp.start_polling(bot)
 
 
-@db.message(aiogram.filters.Command("start"))
+@dp.message(aiogram.filters.Command("start"))
 async def send_welcome(message: aiogram.types.Message):
-    await message.reply("Welcome!")
+    kb = [
+        [
+            aiogram.types.KeyboardButton(text="Информация"),
+            aiogram.types.KeyboardButton(text="Начать!")
+        ]
+    ]
+    keyboard = aiogram.types.ReplyKeyboardMarkup(
+        keyboard = kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите как продолжить"
+    )
+    await message.answer("Добро пожаловать в AnimeGan!", reply_markup=keyboard)
 
-@db.message(aiogram.filters.Command("help"))
+
+
+@dp.message(aiogram.F.text.lower() == "начать!")
+async def photo_or_video_choose(message: aiogram.types.Message):
+    kb = [
+        [
+            aiogram.types.KeyboardButton(text="Фотография"),
+            aiogram.types.KeyboardButton(text="Видео")
+        ]
+    ]
+    keyboard = aiogram.types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Что будем стилизовать?"
+    )
+    await message.answer("Для начала выберите, что будем стилизовать", reply_markup=keyboard)
+
+
+
+@dp.message(aiogram.F.text.lower() == "информация" or aiogram.types.Command("help"))
 async def send_common_information(message: aiogram.types.Message):
+    kb = [
+        [
+            aiogram.types.KeyboardButton(text="Как это работает?"),
+            aiogram.types.KeyboardButton(text="Начать!")
+        ]
+    ]
+    keyboard = aiogram.types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Как продолжим?"
+    )
     await message.answer("Привет!\n\nТы используешь AnimeGAN бота, он создан для стилизации нетяжелых фотографий и коротких видео.\n\n\
-Всё, что тебе нужно это просто выбрать файл. Бот проверит, что он подходит по размеру и качеству, сгенерирует новое, уже стилизованное, изображение!")
+Всё, что тебе нужно это просто выбрать файл. Бот проверит, что он подходит по размеру и качеству, сгенерирует новое, уже стилизованное, изображение!",\
+        reply_markup=keyboard)
+
+
 
 def start_bot():
     asyncio.run(main())
