2330199927d3391360ca062e15420199fe59b2cb
Vlad Ogloblin <igrulichki24@gmail.com>
2024-12-16 10:47:22 +0300

    brackets_delete

--- AnimeGAN/telegram/tg_bot/bot.py
+++ AnimeGAN/telegram/tg_bot/bot.py
@@ -12,11 +12,11 @@ async def main():
     await db.start_polling(bot)
 
 
-@db.message(aiogram.filters.Command(("start")))
+@db.message(aiogram.filters.Command("start"))
 async def send_welcome(message: aiogram.types.Message):
     await message.reply("Welcome!")
 
-@db.message(aiogram.filters.Command(("help")))
+@db.message(aiogram.filters.Command("help"))
 async def send_common_information(message: aiogram.types.Message):
     await message.answer("Привет!\n\nТы используешь AnimeGAN бота, он создан для стилизации нетяжелых фотографий и коротких видео.\n\n\
 Всё, что тебе нужно это просто выбрать файл. Бот проверит, что он подходит по размеру и качеству, сгенерирует новое, уже стилизованное, изображение!")
