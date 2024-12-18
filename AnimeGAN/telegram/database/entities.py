255e994701d4a82da0ce81e1a8a8f73dddb04e2c
Maxim <max.sherb2007@gmail.com>
2024-12-18 11:20:28 +0300

    Added entity for files

--- AnimeGAN/telegram/database/entities.py
+++ AnimeGAN/telegram/database/entities.py
@@ -1,3 +1,6 @@
+import datetime
+
+from sqlalchemy import ForeignKey, DateTime, Column
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -13,3 +16,10 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     videos_left: Mapped[int] = mapped_column(nullable=False, default=5)
     photos_left: Mapped[int] = mapped_column(nullable=False, default=5)
+
+class File(Base):
+    __tablename__ = 'files'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    date = Column(DateTime, default=datetime.datetime.utcnow())
