1bffd9066929ec711d66be15c6ce313645b8a755
Yarik <blanla12345@mail.ru>
2024-12-18 15:47:21 +0300

    add videocheck function

--- AnimeGAN/telegram/tg_bot/procvid.py
+++ AnimeGAN/telegram/tg_bot/procvid.py
@@ -1,5 +1,8 @@
 import cv2
-from PIL import Image 
+from PIL import Image
+import matplotlib.pyplot as plt
+import io
+import os
 
 def crop_video(input_path, output_path):
     cap = cv2.VideoCapture(input_path)
@@ -23,4 +26,18 @@ def crop_video(input_path, output_path):
     cap.release()
     out.release()
 
-crop_video('input_video.mp4', 'output_video.mp4')
+def videocheck(vid):
+    t = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+    fps = vid.get(cv2.CAP_PROP_FPS)
+    vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    f,r=vid.read()
+    if(t/fps>=11.0) or (os.path.getsize(vid)>16*1024**2) or (max(len(r),len(r[0]))<=1024):
+        return False
+    return True
+
+
+def imagecheck(img):
+    image = Image.open(io.BytesIO(img))
+    if(max(image.size())<=1024 and os.path.getsize(img) <= 11*1024*1024):
+        return True
+    return False
