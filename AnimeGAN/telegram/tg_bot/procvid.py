d7f4a4c856dca90a81dc8607497e3bab3c39cba3
Yarik <blanla12345@mail.ru>
2024-12-18 11:48:35 +0300

    add fps limiter

--- AnimeGAN/telegram/tg_bot/procvid.py
+++ AnimeGAN/telegram/tg_bot/procvid.py
@@ -1,30 +1,26 @@
 import cv2
-import matplotlib.pyplot as mplt
+from PIL import Image 
 
-import cv2
-
-def crop_video(input_file, output_file):
-    cap = cv2.VideoCapture(input_file)
-    
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    
+def crop_video(input_path, output_path):
+    cap = cv2.VideoCapture(input_path)
+    fps = 24
+    width = 512
+    height = 512
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(output_file, fourcc, fps, (512, 512))
-    
-    frame_count = 0
-    max_frames = int(fps * 10)
 
-    while cap.isOpened() and frame_count < max_frames:
+    out = cv2.VideoWriter(output_path, fourcc, fps)
+
+    frame_count = 0
+    while cap.isOpened() and frame_count < fps * 10:
         ret, frame = cap.read()
         if not ret:
             break
-        frame_resized = cv2.resize(frame, (512, 512))
-        
-        out.write(frame_resized)
+
+        frame = cv2.thumbnail(frame, (width, height))
+        out.write(frame)
         frame_count += 1
 
     cap.release()
     out.release()
 
+crop_video('input_video.mp4', 'output_video.mp4')
